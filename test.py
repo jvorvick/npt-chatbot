b0b0d09e6112960a2a69081d214dee477e57710c
jvorvick <vorvickjesse@gmail.com>
2023-04-07 18:23:11 +0900

    ai now responds to speech cues and replies with predetermined responses

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import numpy as np
 import speech_recognition as sr
 from gtts import gTTS
 import os
+import datetime
 
 # Build the AI
 class ChatBot():
@@ -16,7 +17,8 @@ class ChatBot():
             audio = recognizer.listen(mic)
         try:
             self.text = recognizer.recognize_google(audio)
-            self.text = "how" in self.text and self.text.replace("how", "HAL")
+            self.text = self.text.replace("how", "HAL") if "how" in self.text else self.text 
+            # self.text = self.text[:1].upper() + self.text[1:]
             print("me --> ", self.text)
         except:
             print("me --> ERROR")
@@ -36,6 +38,10 @@ class ChatBot():
         # os.system("start res.mp3")
 
         os.remove("res.mp3")
+    
+    @staticmethod
+    def action_time():
+        return datetime.datetime.now().time().strftime("%H:%M")
 
 # Execute the AI
 if __name__ == "__main__":
@@ -44,5 +50,12 @@ if __name__ == "__main__":
         ai.speech_to_text()
         ## wake up
         if ai.wake_up(ai.text) is True:
-            res = "I'm afraid I can't do that, Jesse."
+            res = "Hello, I am HAL the AI, what can I do for you?"
+        ## do any action
+        elif "time" in ai.text:
+            res = ai.action_time()
+        ## respond politely
+        elif any(w in ai.text for w in ["thank", "thanks"]):
+            res = np.random.choice(
+                ["you're welcome!", "anytime!", "no problem!", "cool!", "I'm here if you need me!", "peace out!"])
         ai.text_to_speech(res)
